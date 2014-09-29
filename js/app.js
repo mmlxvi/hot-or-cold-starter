65009b632856c88ae43f1cbdf7a545c908d11264
mmlxvi <quarterly24@gmail.com>
2014-09-28 19:44:31 -0500

    add js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,45 @@
-
 $(document).ready(function(){
 	
+	/*--- Generate random number chosen by computer ---*/
+	var num = (Math.random() * 100);
+	var computerChoice = num.toFixed(0);
+	var userChoice;
+	console.log(computerChoice);
+
+	/*--- User can start new game without reloading page ---*/
+	$(".new").click(function() {
+		num = (Math.random() * 100);
+		computerChoice = num.toFixed(0);
+		console.log(computerChoice);
+		});
+    
+
+    $("#userGuess").keydown(function(e){
+
+    	if(e.keyCode == 13) {
+		
+    		userChoice = document.getElementById("userGuess").value;
+    
+		}
+            console.log(userChoice);
+
+        });
+        
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+    	$(".overlay").fadeIn(700);
 
   	});
 
   	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
+  		$(".overlay").fadeOut(700);
   	});
 
+  	
+
+
+
+
 });
-
-
