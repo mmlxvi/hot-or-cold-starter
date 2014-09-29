aebc917421e63ecc24589cfafe18a317e96a1872
mmlxvi <quarterly24@gmail.com>
2014-09-28 23:35:22 -0500

    adds

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 	
+	
+	var userChoice;
+	var computerChoice;
+	
 	/*--- Generate random number chosen by computer ---*/
 	var num = (Math.random() * 100);
 	var computerChoice = num.toFixed(0);
@@ -7,18 +11,66 @@ $(document).ready(function(){
 	console.log(computerChoice);
 
 	/*--- User can start new game without reloading page ---*/
-	$(".new").click(function() {
+	
+    
+
+    $("#guessButton").click(function(){
+    	userChoice = document.getElementById("userGuess").value;
+    	var difference = (Math.abs(computerChoice - userChoice));
+    	
+    	if (isNaN(userChoice)) {
+    		alert("\"" + userChoice + "\"" + " is not a number. Please enter a number between 1 and 100.");
+    	}
+
+    	else if ((userChoice < 1) || (userChoice > 100)) {
+    		alert("Enter a number between 1 and 100.");
+    	}
+    	
+    	else if (userChoice === computerChoice) {
+    		alert("You guessed it!");
+    	}
+
+    	else if (difference <= 3) {
+    		alert("Smoking hot!");
+    	}
+
+    	else if (difference > 3 && difference <= 6) {
+    		alert("Hot!");
+    	}
+
+    	else if (difference > 6 && difference <= 12) {
+    		alert("More than warm...");
+    	}
+
+    	else if (difference > 12 && difference <= 20) {
+    		alert("Warm!");
+    	}
+
+    	else if (difference > 20 && difference <= 30) {
+    		alert("Kinda warm...");
+    	}
+
+    	else if (difference > 30 && difference <= 50) {
+    		alert("Cold.");
+    	}
+
+    	else if (difference > 50 && difference <= 75) {
+    		alert("Ice cold.");
+    	}
+
+    	else {
+    		alert("You're on Antarctica.");
+    	};
+
+        });
+    
+ 
+
+    $(".new").click(function() {
 		num = (Math.random() * 100);
 		computerChoice = num.toFixed(0);
 		console.log(computerChoice);
 		});
-    
-
-    $("#guessButton").click(function(){
-    	alert('button works');
-    	
-        });
-        
 
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
