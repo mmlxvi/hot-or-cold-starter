8696f4e81e32c6f1475c44860e562159691a0661
mmlxvi <quarterly24@gmail.com>
2014-09-30 15:26:49 -0500

    add

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	
 	var userChoice;
 	var computerChoice;
-	
+	var difference;
 	/*--- Generate random number chosen by computer ---*/
 	var num = (Math.random() * 100);
 	var computerChoice = num.toFixed(0);
@@ -12,58 +12,66 @@ $(document).ready(function(){
 
 	/*--- User can start new game without reloading page ---*/
 	
-    
+   
 
     $("#guessButton").click(function(){
     	userChoice = document.getElementById("userGuess").value;
-    	var difference = (Math.abs(computerChoice - userChoice));
+    	difference = (Math.abs(computerChoice - userChoice));
     	$('#guessButton').val('');
-
-    	while (userChoice !== computerChoice) {
-
-    	    	if (isNaN(userChoice)) {
+    	
+    	
+    	if (isNaN(userChoice)) {
     		alert("\"" + userChoice + "\"" + " is not a number. Please enter a number between 1 and 100.");
     	}
 
     	else if ((userChoice < 1) || (userChoice > 100)) {
     		alert("Enter a number between 1 and 100.");
     	}
+
+    	else if (userChoice === computerChoice) {
+    		alert("You guessed it!");
+    	}
     	
-    	else if (difference <= 3) {
-    		alert("Smoking hot!");
-    	}
-
-    	else if (difference > 3 && difference <= 6) {
-    		alert("Hot!");
-    	}
-
-    	else if (difference > 6 && difference <= 12) {
-    		alert("More than warm...");
-    	}
-
-    	else if (difference > 12 && difference <= 20) {
-    		alert("Warm!");
-    	}
-
-    	else if (difference > 20 && difference <= 30) {
-    		alert("Kinda warm...");
-    	}
-
-    	else if (difference > 30 && difference <= 50) {
-    		alert("Cold.");
-    	}
-
-    	else if (difference > 50 && difference <= 75) {
-    		alert("Ice cold.");
-    	}
-
     	else {
-    		alert("You're on Antarctica.");
-    	};
 
-        };
+    			if (difference > 0 && difference <= 3) {
+    			alert("Smoking hot!");
+    			}
+
+	    		else if (difference > 3 && difference <= 6) {
+	    		alert("Hot!");
+	    		}
+
+	    		else if (difference > 6 && difference <= 12) {
+	    		alert("More than warm...");
+	    		}
+
+	    		else if (difference > 12 && difference <= 20) {
+	    		alert("Warm!");
+	    		}
+
+	    		else if (difference > 20 && difference <= 30) {
+	    		alert("Kinda warm...");
+	    		}
+
+	    		else if (difference > 30 && difference <= 50) {
+	    		alert("Cold.");
+	    		}
+
+	    		else if (difference > 50 && difference <= 75) {
+	    		alert("Very cold.");
+	    		}
+
+	    		else {
+	    		alert("Freezing.");
+	    		}
+    		
+    		}
+       	
     	
-    	});
+    });
+
+
  
 
     $(".new").click(function() {
