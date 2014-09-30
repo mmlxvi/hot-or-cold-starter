1e2331fdc5b77f559b293b207fc27569638013dc
mmlxvi <quarterly24@gmail.com>
2014-09-30 17:15:39 -0500

    add

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 	var userChoice;
 	var computerChoice;
 	var difference;
+	var counter=0;
 	/*--- Generate random number chosen by computer ---*/
 	var num = (Math.random() * 100);
 	var computerChoice = num.toFixed(0);
@@ -12,12 +13,14 @@ $(document).ready(function(){
 
 	/*--- User can start new game without reloading page ---*/
 	
-   
+for (counter = 0; counter < 100; counter++) {
 
     $("#guessButton").click(function(){
     	userChoice = document.getElementById("userGuess").value;
     	difference = (Math.abs(computerChoice - userChoice));
     	$('#guessButton').val('');
+
+    	$("#guessList").append(userChoice + ",");
     	
     	
     	if (isNaN(userChoice)) {
@@ -35,65 +38,79 @@ $(document).ready(function(){
     	else {
 
     			if (difference > 0 && difference <= 3) {
-    			alert("Smoking hot!");
+    			$("#feedback").text("You're very hot!");
     			}
 
 	    		else if (difference > 3 && difference <= 6) {
+	    		
+	    		$("#feedback").text("Hot!");
 	    		alert("Hot!");
+	    		
 	    		}
 
 	    		else if (difference > 6 && difference <= 12) {
+	    		
+	    		$("#feedback").text("More than warm...");
 	    		alert("More than warm...");
+	    		
 	    		}
 
 	    		else if (difference > 12 && difference <= 20) {
-	    		alert("Warm!");
+	    		
+	    		$("#feedback").text("Warm.");
+	    		alert("Warm.");
 	    		}
 
 	    		else if (difference > 20 && difference <= 30) {
+	    		
+	    		$("#feedback").text("Kinda warm...");
 	    		alert("Kinda warm...");
 	    		}
 
 	    		else if (difference > 30 && difference <= 50) {
+	    		
+	    		$("#feedback").text("Cold.");
 	    		alert("Cold.");
 	    		}
 
 	    		else if (difference > 50 && difference <= 75) {
+	    		
+	    		$("#feedback").text("Very cold.");
 	    		alert("Very cold.");
 	    		}
 
 	    		else {
+	    		
+	    		$("#feedback").text("Frickin' freezing.");
 	    		alert("Freezing.");
 	    		}
     		
     		}
-       	
+    	
     	
     });
 
-
+}
  
-
+/*
     $(".new").click(function() {
 		num = (Math.random() * 100);
 		computerChoice = num.toFixed(0);
 		console.log(computerChoice);
-		});
+		}); */
 
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(700);
+  	//$(".what").click(function(){
+    //	$(".overlay").fadeIn(700);
 
-  	});
+  	//});
 
   	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(700);
-  	});
+  	//$("a.close").click(function(){
+  	//	$(".overlay").fadeOut(700);
+  	//});
 
   	
-
-
 
 
 });
